57bf583f7721fb59b5f6390f148a09080648c385
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2022-08-17 14:55:49 +0200

    Add basket page into the app

---
+++ app/src/main/java/com/example/petshopapp/item_basket.java
@@ -0,0 +1,14 @@
+package com.example.petshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_basket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_basket);
+    }
+}
