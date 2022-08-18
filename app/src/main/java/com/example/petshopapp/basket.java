023480f8831ff8f3750f5f1042749ff903d8a782
Earth Kuma <kruthai2540@gmail.com>
2022-08-18 17:05:51 +0200

    Change basket page view

--- app/src/main/java/com/example/petshopapp/basket.java
+++ app/src/main/java/com/example/petshopapp/basket.java
@@ -6,19 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class basket extends AppCompatActivity implements basket_cardViewAdapter.ItemClickListener {
-
-    basket_cardViewAdapter adapter;
-    TextView name, prices;
-
-    ArrayList<String> basketItemNames = new ArrayList<>();
-    ArrayList<Integer> basketItemPrices = new ArrayList<>();
+public class basket extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +24,13 @@ public class basket extends AppCompatActivity implements basket_cardViewAdapter.
         basket_dbHandler db = new basket_dbHandler(this);
         ArrayList<HashMap<String, String>> itemList = db.GetItems();
 
-        name = (TextView)findViewById(R.id.tvBasketItemName);
-        prices = (TextView)findViewById(R.id.tvBasketItemPrice);
+        ListView lv = (ListView) findViewById(R.id.item_list);
+        ListAdapter adapter = new SimpleAdapter(basket.this, itemList, R.layout.list_row,
+                new String[]{"name", "price"},
+                new int[]{R.id.lvItemName, R.id.lvItemPrice});
 
+        lv.setAdapter(adapter);
 
-
-        RecyclerView recyclerView = findViewById(R.id.rvBasketItems);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new basket_cardViewAdapter(this, basketItemNames, basketItemPrices);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-
-    }
-
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
 
     }
 }
